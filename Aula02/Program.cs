bda5fa9a66119e3a5d7bb84dc6eeba68cabb1c4c
brenda <breendasouusaa@gmail.com>
2020-03-10 17:59:46 -0300

    criação

---
+++ Aula02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bimestre01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = { "Adryan Alencar", "Beatriz Minha Linda<3" };
+
+            foreach (string item in nomes)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
